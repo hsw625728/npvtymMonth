b825fb24a02287d772b1702e6072f885c9f4ef62
PangXieOffice <hsw625728@163.com>
2017-07-18 19:30:18 +0800

    付费包月功能制作完毕
    功能制作完毕，还需进行多次测试调试。

--- VpnTest/VpnTest/CustomViews/MLBUserHomeHeaderView.m
+++ VpnTest/VpnTest/CustomViews/MLBUserHomeHeaderView.m
@@ -119,6 +119,14 @@
 - (void)handleSingleTap:(UIGestureRecognizer *)gestureRecognizer {
     AppDelegate *appDelegate = (AppDelegate*)[[UIApplication sharedApplication] delegate];
     
+    //包月验证标记开始
+    //检测本地存储的包月凭证
+    int statue = [self checkTime];
+    if (statue == 0)
+    {
+        return;
+    }
+    //包月验证标记结束
     switch (appDelegate.gStatue) {
         case VPN_DISCONNECTED:
             [[MoVPNManage shareVPNManage] vpnStart];
@@ -140,6 +148,118 @@
     //[[MoVPNManage shareVPNManage] vpnStop];
     //_nicknameLabel.text = @"连接成功";
     //do something....
+}
+
+- (int)checkTime{
+    
+    // 1.需要知道这个对象存在哪里，所以需要一个文件夹的路径
+    // 找到Documents文件夹路径
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    
+    // 2.创建要存储的内容：字符串
+    //NSString *str = @"AJAR";
+    // 3.需要知道字符串最终存储的地方，所以需要创建一个路径去存储字符串
+    NSString *strPath = [documentsPath stringByAppendingPathComponent:@"mengyoutu_vip_time"];
+    
+    // 4.将字符串写入文件
+    // 第一个参数：写入的文件的一个路径
+    // 第二个参数：编码方式
+    // 第三个参数：错误信息
+    //[str writeToFile:strPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    
+#pragma mark - 将NSString文件夹存储的内容取出来
+    // 将字符串取出,使用stringWithContentsOfFile这个方法将其取出
+    // 第一个参数：字符串存储的路径
+    // 第二个参数：编码方式
+    // 第三个参数：错误信息
+    NSString *newStr = [NSString stringWithContentsOfFile:strPath encoding:NSUTF8StringEncoding error:nil];
+    if (newStr == nil){
+        NSLog(@"没有找到包月凭证。newStr = %@", newStr);
+        UIAlertView *alert =[[UIAlertView alloc] initWithTitle:@"授权过期"
+                                                       message:@"使用授权已过期，请点击续费包月菜单购买授权，或点击“恢复授权”激活已购买的授权。"
+                                                      delegate:nil
+                                             cancelButtonTitle:@"确定"
+                                             otherButtonTitles:nil];
+        [alert show];
+        return 0;
+    }else{
+        NSLog(@"找到包月凭证。过期时间 = %@", newStr);
+        
+        
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss Etc/GMT"];
+        NSDate *date = [dateFormatter dateFromString:newStr];
+        NSLog(@"过期时间:%@", newStr);
+        
+        
+        NSDateFormatter *dateFormatter1 = [[NSDateFormatter alloc] init];
+        [dateFormatter1 setDateFormat:@"yyyy-MM-dd HH:mm:ss Etc/GMT"];
+        NSString *oneDayStr1 = [dateFormatter1 stringFromDate:[self getCurrentTime]];
+        NSLog(@"现在时间:%@", oneDayStr1);
+        int test = [newStr compare:oneDayStr1];//[self compareOneDay:[self getCurrentTime] withAnotherDay:date];
+        NSLog(@"比较结果：%i", test);
+        if (test < 0)
+        {
+            UIAlertView *alert =[[UIAlertView alloc] initWithTitle:@"授权过期"
+                                                           message:@"使用授权已过期，请点击续费包月菜单购买授权，或点击“恢复授权”激活已购买的授权。"
+                                                          delegate:nil
+                                                 cancelButtonTitle:@"确定"
+                                                 otherButtonTitles:nil];
+            [alert show];
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+}
+
+- (NSDate *)getCurrentTime{
+    /*
+    NSDateFormatter *formatter=[[NSDateFormatter alloc]init];
+    [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss Etc/GMT"];
+    NSString *dateTime=[formatter stringFromDate:[NSDate date]];
+    NSDate *date = [formatter dateFromString:dateTime];
+    */
+    NSDate *date = [NSDate date];
+    
+    NSTimeZone *tzGMT = [NSTimeZone timeZoneWithName:@"GMT"];
+    [NSTimeZone setDefaultTimeZone:tzGMT];
+    
+    NSDateFormatter *iosDateFormater=[[NSDateFormatter alloc]init];
+    
+    iosDateFormater.dateFormat=@"yyyy-MM-dd HH:mm:ss Etc/GMT'";
+    
+    iosDateFormater.locale=[[NSLocale alloc]initWithLocaleIdentifier:@"zh_CN"];
+    NSString *dateStr = [iosDateFormater stringFromDate:date];
+    //NSLog(@"---------- currentDate == %@",dateStr);
+    
+    return date;
+}
+
+- (int)compareOneDay:(NSDate *)oneDay withAnotherDay:(NSDate *)anotherDay
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss Etc/GMT"];
+    NSString *oneDayStr = [dateFormatter stringFromDate:oneDay];
+    NSString *anotherDayStr = [dateFormatter stringFromDate:anotherDay];
+    NSDate *dateA = [dateFormatter dateFromString:oneDayStr];
+    NSDate *dateB = [dateFormatter dateFromString:anotherDayStr];
+    NSComparisonResult result = [dateA compare:dateB];
+    NSLog(@"date1 : %@, date2 : %@", oneDay, anotherDay);
+    if (result == NSOrderedDescending) {
+        //NSLog(@"Date1  is in the future");
+        return 1;
+    }
+    else if (result == NSOrderedAscending){
+        //NSLog(@"Date1 is in the past");
+        return -1;
+    }
+    //NSLog(@"Both dates are the same");
+    return 0;
+    
 }
 
 - (CGFloat)viewHeight {
--- VpnTest/VpnTest/ViewControllers/MLBIAP.m
+++ VpnTest/VpnTest/ViewControllers/MLBIAP.m
@@ -11,6 +11,9 @@
 #import "MLBSettingsCell.h"
 #import "IAPShare.h"
 #import "IAPHelper.h"
+#import <UIKit/UIKit.h>
+#import "NSString+Base64.h"
+//#import <Foundation/NSJSONSerialization.h>
 
 @interface MLBIAPViewController () <UITableViewDataSource, UITableViewDelegate>
 
@@ -25,6 +28,7 @@
     SKProduct* product0;
     SKProduct* product1;
     SKProduct* product2;
+    UIActivityIndicatorView* activityIndicatorView;
 }
 
 #pragma mark - Lifecycle
@@ -56,7 +60,7 @@
 #pragma mark - Private Method
 
 - (void)initDatas {
-    dataSource = @[@[@"订阅", @[@"45元订阅一个月", @"268元订阅六个月", @"508元订阅一年"]]];
+    dataSource = @[@[@"订阅", @[@"45元订阅一个月", @"268元订阅六个月", @"508元订阅一年"]], @[@"恢复授权", @[@"恢复授权"]]];
     
     NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
 //    NSString *prodName = [infoDictionary objectForKey:@"CFBundleName"];
@@ -72,6 +76,16 @@
     //yes为生产环境  no为沙盒测试模式
     // 客户端做收据校验有用  服务器做收据校验忽略...
     [IAPShare sharedHelper].iap.production = NO;
+    
+    //网络请求等待动画
+    self->activityIndicatorView=[[UIActivityIndicatorView alloc]initWithFrame:CGRectMake(0,0,100,100)];
+    self->activityIndicatorView.center=self.view.center;
+    [self->activityIndicatorView setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleGray];
+    [self->activityIndicatorView setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    [self->activityIndicatorView setBackgroundColor:ColorWithRGBA(100, 100, 100, 100)];
+    [self.view addSubview:self->activityIndicatorView];
+    [self->activityIndicatorView startAnimating];
+    
     [[IAPShare sharedHelper].iap requestProductsWithCompletion:^(SKProductsRequest* request,SKProductsResponse* response)
      {
          if(response > 0 ) {
@@ -80,8 +94,9 @@
              product1 =[[IAPShare sharedHelper].iap.products objectAtIndex:1];
              product2 =[[IAPShare sharedHelper].iap.products objectAtIndex:2];
              
-             
+             NSLog(@"SUCCESS %@",response);
          }
+         [self->activityIndicatorView stopAnimating];
      }];
 }
 
@@ -160,6 +175,10 @@
         default:
             break;
     }
+    
+    if (nil == product){
+        return ;
+    }
     NSLog(@"Price: %@",[[IAPShare sharedHelper].iap getLocalePrice:product]);
     NSLog(@"Title: %@",product.localizedTitle);
     
@@ -171,8 +190,40 @@
                                        NSLog(@"Fail %@",[trans.error localizedDescription]);
                                    }
                                    else if(trans.transactionState == SKPaymentTransactionStatePurchased) {
+                                       /*
+                                       //收据整理上传服务器
+                                       // 这个 receipt 就是内购成功 苹果返回的收据
+                                       NSData *receipt = [NSData dataWithContentsOfURL:[[NSBundle mainBundle] appStoreReceiptURL]];
                                        
-                                       [[IAPShare sharedHelper].iap checkReceipt:[NSData dataWithContentsOfURL:[[NSBundle mainBundle] appStoreReceiptURL]] AndSharedSecret:@"your sharesecret" onCompletion:^(NSString *response, NSError *error) {
+                                       
+                                       
+                                       //******这里我将receipt base64加密，把加密的收据 和 产品id，一起发送到app服务器********
+                                       NSString *receiptBase64 = [NSString base64StringFromData:receipt length:[receipt length]];
+                                       
+                                       //上传服务器
+                                       //[self sendCheckReceiptWithBase64:receiptBase64 productID:product.productIdentifier];
+                                       NSString *idfv = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+                                       NSString *post = [NSString stringWithFormat:@"deviceID=%@&record=%@",idfv, receiptBase64];
+                                       
+                                       NSData *postData = [post dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+                                       
+                                       NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+                                       NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+                                       [request setURL:[NSURL URLWithString:@"http://mengyoutu.cn/npvtymmonth/vpnmonthrecord.php"]];
+                                       [request setHTTPMethod:@"POST"];
+                                       [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+                                       [request setValue:@"application/x-www-form-urlencoded;charset=UTF-8" forHTTPHeaderField:@"Content-Type"];
+                                       [request setHTTPBody:postData];
+                                       
+                                       NSURLResponse *response;
+                                       NSData *POSTReply = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
+                                       NSString *theReply = [[NSString alloc] initWithBytes:[POSTReply bytes] length:[POSTReply length] encoding: NSASCIIStringEncoding];
+                                       NSLog(@"& API  | %@", theReply);
+                                       
+                                       */
+                                       //收据上传服务器完毕
+                                       // 本地验证
+                                       [[IAPShare sharedHelper].iap checkReceipt:[NSData dataWithContentsOfURL:[[NSBundle mainBundle] appStoreReceiptURL]] AndSharedSecret:@"0013c2deada24497a5a699390d7ca25a" onCompletion:^(NSString *response, NSError *error) {
                                            
                                            //Convert JSON String to NSDictionary
                                            NSDictionary* rec = [IAPShare toJSON:response];
@@ -181,16 +232,36 @@
                                            {
                                                
                                                [[IAPShare sharedHelper].iap provideContentWithTransaction:trans];
-                                               NSLog(@"SUCCESS %@",response);
+                                               //NSLog(@"SUCCESS %@",response);
                                                NSLog(@"Pruchases %@",[IAPShare sharedHelper].iap.purchasedProducts);
+                                               
+                                               //包月验证标记开始
+                                               //写入本地存储的包月凭证
+                                               // 1.需要知道这个对象存在哪里，所以需要一个文件夹的路径
+                                               // 找到Documents文件夹路径
+                                               NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+                                               
+                                               // 2.创建要存储的内容：字符串
+                                               NSString *str = [self parseExpiresDate:response];//@"这里以后填写到期时间等信息";
+                                               NSLog(@"ExpriesTime %@",str);
+                                               // 3.需要知道字符串最终存储的地方，所以需要创建一个路径去存储字符串
+                                               NSString *strPath = [documentsPath stringByAppendingPathComponent:@"mengyoutu_vip_time"];
+                                               
+                                               // 4.将字符串写入文件
+                                               // 第一个参数：写入的文件的一个路径
+                                               // 第二个参数：编码方式
+                                               // 第三个参数：错误信息
+                                               [str writeToFile:strPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+                                               //包月验证标记结束
                                            }
                                            else {
-                                               NSLog(@"Fail");
+                                               NSLog(@"CheckFail");
                                            }
                                        }];
+                                       //本地验证结束
                                    }
                                    else if(trans.transactionState == SKPaymentTransactionStateFailed) {
-                                       NSLog(@"Fail");
+                                       NSLog(@"StateFail");
                                    }
                                }];//end of buy product
 }
@@ -221,9 +292,122 @@
         [self buyItem:1];
     } else if (indexPath.section == 0 && indexPath.row == 2) {
         [self buyItem:2];
+    } else {
+        [[IAPShare sharedHelper].iap restoreProductsWithCompletion:^(SKPaymentQueue *payment, NSError *error) {
+            
+            //check with SKPaymentQueue
+            
+            // number of restore count
+            int numberOfTransactions = payment.transactions.count;
+            
+            for (SKPaymentTransaction *transaction in payment.transactions)
+            {
+                NSString *purchased = transaction.payment.productIdentifier;
+                //if([purchased isEqualToString:@"cn.mengyoutu.vpn.month1"])
+                {
+                    //enable the prodcut here
+                    NSLog(@"授权回复成功%@", purchased);
+                    
+                    //包月验证标记开始
+                    //写入本地存储的包月凭证
+                    // 1.需要知道这个对象存在哪里，所以需要一个文件夹的路径
+                    // 找到Documents文件夹路径
+                    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+                    
+                    // 2.创建要存储的内容：字符串
+                    NSString *str = @"这里以后填写到期时间等信息";
+                    // 3.需要知道字符串最终存储的地方，所以需要创建一个路径去存储字符串
+                    NSString *strPath = [documentsPath stringByAppendingPathComponent:@"mengyoutu_vip_time"];
+                    
+                    // 4.将字符串写入文件
+                    // 第一个参数：写入的文件的一个路径
+                    // 第二个参数：编码方式
+                    // 第三个参数：错误信息
+                    [str writeToFile:strPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+                    //包月验证标记结束
+                    
+                    [[IAPShare sharedHelper].iap checkReceipt:[NSData dataWithContentsOfURL:[[NSBundle mainBundle] appStoreReceiptURL]] AndSharedSecret:@"0013c2deada24497a5a699390d7ca25a" onCompletion:^(NSString *response, NSError *error) {
+                        
+                        //Convert JSON String to NSDictionary
+                        NSDictionary* rec = [IAPShare toJSON:response];
+                        
+                        if([rec[@"status"] integerValue]==0)
+                        {
+                            
+                            //[[IAPShare sharedHelper].iap provideContentWithTransaction:trans];
+                            //NSLog(@"SUCCESS %@",response);
+                            NSLog(@"Pruchases %@",[IAPShare sharedHelper].iap.purchasedProducts);
+                            
+                            //包月验证标记开始
+                            //写入本地存储的包月凭证
+                            // 1.需要知道这个对象存在哪里，所以需要一个文件夹的路径
+                            // 找到Documents文件夹路径
+                            NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+                            
+                            // 2.创建要存储的内容：字符串
+                            
+                            NSString *str = [self parseExpiresDate:response];//@"这里以后填写到期时间等信息";
+                            NSLog(@"ExpriesTime %@",str);
+                            // 3.需要知道字符串最终存储的地方，所以需要创建一个路径去存储字符串
+                            NSString *strPath = [documentsPath stringByAppendingPathComponent:@"mengyoutu_vip_time"];
+                            
+                            // 4.将字符串写入文件
+                            // 第一个参数：写入的文件的一个路径
+                            // 第二个参数：编码方式
+                            // 第三个参数：错误信息
+                            [str writeToFile:strPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+                            //包月验证标记结束
+                        }
+                        else {
+                            NSLog(@"CheckFail:%li", [rec[@"status"] integerValue]);
+                        }
+                    }];
+                }
+            }
+            
+            
+            UIAlertView *alert =[[UIAlertView alloc] initWithTitle:@"授权恢复完毕"
+                                                           message:@"授权恢复流程执行完毕"
+                                                          delegate:nil
+                                                 cancelButtonTitle:@"确定"
+                                                 otherButtonTitles:nil];
+            [alert show];
+        }];
+        
+        
+        
     }
-}
 
+}
+- (NSString*) parseExpiresDate:(NSString*)data{
+    NSLog(@"data %@",data);
+    NSData *jsonData= [data dataUsingEncoding:NSASCIIStringEncoding];
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:nil];
+
+    NSArray *rowsArr = dict[@"latest_receipt_info"];
+    NSDictionary *dit = rowsArr[rowsArr.count-1];
+    
+    NSString* expires_date = dit[@"expires_date"];
+    return expires_date;
+}
+/*
+ func expirationDateFromResponse(jsonResponse: NSDictionary) → NSDate? {
+ 
+ if let receiptInfo: NSArray = jsonResponse["latest_receipt_info"] as? NSArray {
+ 
+ let lastReceipt = receiptInfo.lastObject as! NSDictionary
+ var formatter = NSDateFormatter()
+ formatter.dateFormat = "yyyy-MM-dd HH:mm:ss VV"
+ 
+ let expirationDate: NSDate =
+ formatter.dateFromString(lastReceipt["expires_date"] as! String) as NSDate!
+ 
+ return expirationDate
+ } else {
+ return nil
+ }
+ }
+ */
 - (void)DoneClick
 {
     [self dismissViewControllerAnimated:YES completion:nil];
